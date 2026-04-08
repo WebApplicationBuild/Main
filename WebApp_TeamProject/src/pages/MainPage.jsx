83c93a4e53b9b1ac7262f06766d9bd4daba62001
meansung <potmean0227@gmail.com>
2026-04-09 02:25:44 +0900

    page 4개 생성 후 테스트 완료
    react-router-dom 다운로드,
    MainPage, LoginPage, MatchingPage, ProjectManagePage 생성 후 연결 확인 완료

---
+++ WebApp_TeamProject/src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+export default function MainPage() {
+    return(
+        <>
+        <main>
+            {/*핵심 콘텐츠(페이지의 실제 기능들)*/}
+            <h1>메인 페이지</h1>
+            <p>핵심 내용</p>
+        </main>
+        </>
+    );
+}
