a836de06443a50060b8cbb7503b337dff6a11c88
daeseong0316 <dsh1345@naver.com>
2026-05-16 17:51:16 +0900

    Revert "Merge pull request #3 from WebApplicationBuild/feature/login"
    This reverts commit 8ed8f927d7d308ff3782e59f934055a42f0bf34d, reversing
    changes made to 30a58b6fb47a1e45adeff9d1546c9ca96f905168.

--- WebApp_TeamProject/src/pages/MatchingPage.jsx
+++
@@ -1,7 +0,0 @@
-export default function MatchingPage() {
-    return(
-        <div>
-            <h1>이 곳은 매칭페이지입니다.</h1>
-        </div>
-    );
-}
--- WebApp_TeamProject/src/store/AuthContext.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-/**
- Context 객체만 별도 파일로 분리 -> 건드리지말것!
- */
-export const AuthContext = createContext(null);
